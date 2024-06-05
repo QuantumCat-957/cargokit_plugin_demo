87dee947cab81daac1a4ce65a653c3d5f872379c
wenjing <wenjing@company.com>
2024-06-05 19:28:05 +0800

    initial commit

---
+++ example/android/app/src/main/kotlin/com/example/test_cargokit_ffi_plugin_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.test_cargokit_ffi_plugin_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ ios/Classes/test_cargokit_ffi_plugin.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../{projectName}}.podspec for more information.
+#include "../../src/test_cargokit_ffi_plugin.c"
